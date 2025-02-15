c667709b7a47c56764074f1d80e34853091b0399
Sunsanee Persson <3kdyhfe24.perssu@folkuniversitetet.nu>
2025-02-15 17:26:37 +0100

    Crated local Starage Js file

--- template/scripts/script.js
+++ template/scripts/script.js
@@ -14,39 +14,5 @@ if(window.location.pathname === '/' || window.location.pathname === '/index.html
 
 }
 
-import { fetchTopMovies } from './modules/api.js';
-
-// Anropa funktionen
-fetchTopMovies().then(data => {
-    console.log(data);
-    // Gör något med den hämtade datan
-});
-
-randomMovies();
-favoritemovies();
-movieDatails();
-
-function randomMovies() {
-    console.log(randomMovies);
-    
-
-async function main() {
-    await fetchTopMovies();
-    console.log(oData.topMovieList); // Skriv ut den uppdaterade filmlistan
-}
-
-main();
-
-    
-    
-}
 
 
-function favoritemovies() {
-    console.log(favoritemovies);
-    
-}
-function movieDatails() {
-    console.log(movieDatails);
-    
-}
