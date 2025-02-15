dc9165e062e870cbe5419c8cecd85d2f5e459a55
Sunsanee Persson <3kdyhfe24.perssu@folkuniversitetet.nu>
2025-02-15 13:37:20 +0100

    Crated data.js

--- template/scripts/script.js
+++ template/scripts/script.js
@@ -1,4 +1,5 @@
 
+
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
 
@@ -11,4 +12,41 @@ if(window.location.pathname === '/' || window.location.pathname === '/index.html
 } else if(window.location.pathname === '/search.html') {
     console.log('search.html');
 
+}
+
+import { fetchTopMovies } from './modules/api.js';
+
+// Anropa funktionen
+fetchTopMovies().then(data => {
+    console.log(data);
+    // Gör något med den hämtade datan
+});
+
+randomMovies();
+favoritemovies();
+movieDatails();
+
+function randomMovies() {
+    console.log(randomMovies);
+    
+
+async function main() {
+    await fetchTopMovies();
+    console.log(oData.topMovieList); // Skriv ut den uppdaterade filmlistan
+}
+
+main();
+
+    
+    
+}
+
+
+function favoritemovies() {
+    console.log(favoritemovies);
+    
+}
+function movieDatails() {
+    console.log(movieDatails);
+    
 }
